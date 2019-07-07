055ba5997e9a9ffacc31dfd021286f9db03cc1d1
Tomislav Podunajec <tpodunaje@foi.hr>
2019-07-07 10:12:46 +0200

    Napravljena početna hijerarhija klasa. (UI_Element, Card, Button).

--- Test Raylib CS/Program.cs
+++ Test Raylib CS/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Raylib;
-using rl = Raylib.Raylib;
+using static Raylib.Raylib;
 
 namespace Memory
 {
@@ -19,76 +19,99 @@ namespace Memory
 
     class UI_Element
     {
-        public UI_Element(int x, int y, int w, int h, string text, GameWindow window)
+        public UI_Element(int x, int y, int w, int h, string text)
         {
             rect = new Rectangle(x - (w / 2), y, w, h);
             this.text = text;
             color = Color.BLUE;
-            this.window = window;
         }
 
         public void DrawMe()
         {
-            rl.DrawRectangleRec(rect, color);
-            rl.DrawText(text, (int)rect.x + text.Length, (int)rect.y + ((int)rect.height / 3), (int)rect.height / 2, Color.RED);
+            DrawRectangleRec(rect, color);
+            DrawText(text, (int)rect.x + text.Length, (int)rect.y + ((int)rect.height / 3), (int)rect.height / 2, Color.RED);
         }
 
-        public void CheckIfClicked(ref GameWindow window)
+        public bool CheckIfClicked()
         {
-            if (rl.CheckCollisionPointRec(rl.GetMousePosition(), rect))
-            {
-                if (rl.IsMouseButtonPressed(0))
-                {
-                    window = this.window;
-                }
-            }
+            return CheckCollisionPointRec(GetMousePosition(), rect) && IsMouseButtonPressed(0);
         }
 
-        private Rectangle rect;
-        private string text;
-        private Color color;
-        private GameWindow window;
+        protected Rectangle rect;
+        protected string text;
+        protected Color color;
+        
+    }
+
+    class Card : UI_Element
+    {
+        public Card(int x, int y, int w, int h, string text, string fileName = "") : base(x, y, w, h, text)
+        {
+
+        }
+
+        
+    }
+
+    class Button : UI_Element
+    {
+        public Button(int x, int y, int w, int h, string text, GameWindow window) : base(x, y, w, h, text)
+        {
+            this.Window = window;
+        }
+
+        public GameWindow Window { get; private set; }
     }
 
     class Program
     {
-        static List<UI_Element> menuItems;
-        static List<UI_Element> memCards;
-        static UI_Element toMenu;
+        static List<Button> menuItems;
+        static List<Card> memCards;
+        static Button toMenu;
         static GameWindow gameWindow;
 
         static void DrawMenuWindow()
         {
             menuItems.ForEach(rect => rect.DrawMe());
-            menuItems.ForEach(x => x.CheckIfClicked(ref gameWindow));
+
+            foreach (var item in menuItems)
+            {
+                if (item.CheckIfClicked())
+                {
+                    gameWindow = item.Window;
+                }
+            }
         }
 
         static void DrawGameWindow()
         {
             memCards.ForEach(card => card.DrawMe());
             toMenu.DrawMe();
-            toMenu.CheckIfClicked(ref gameWindow);
+            if (toMenu.CheckIfClicked())
+            {
+                gameWindow = toMenu.Window;
+            }
         }
 
         static void Main(string[] args)
         {
-            rl.InitWindow(640, 480, "Memory");
-            UI_Element startRect = new UI_Element(320, 215, 150, 50, "Start Game", GameWindow.Game);
-            UI_Element optionsRect = new UI_Element(320, 280, 150, 50, "Options", GameWindow.Options);
-            UI_Element quitRect = new UI_Element(320, 345, 150, 50, "Quit", GameWindow.Quit);
-            toMenu = new UI_Element(590, 460, 100, 20, "Back", GameWindow.Menu);
+            InitWindow(640, 480, "Memory");
+            var startRect = new Button(320, 215, 150, 50, "Start Game", GameWindow.Game);
+            var optionsRect = new Button(320, 280, 150, 50, "Options", GameWindow.Options);
+            var quitRect = new Button(320, 345, 150, 50, "Quit", GameWindow.Quit);
+            toMenu = new Button(590, 460, 100, 20, "Back", GameWindow.Menu);
 
-            menuItems = new List<UI_Element> { startRect, optionsRect, quitRect };
+            menuItems = new List<Button> { startRect, optionsRect, quitRect };
             gameWindow = GameWindow.Menu;
 
-            memCards = new List<UI_Element>();
+            memCards = new List<Card>();
 
             int x = 95;
             int y = 20;
 
             for (int i = 0; i < 16; i++)
             {
-                memCards.Add(new UI_Element(x, y, 140, 100, "CARD", GameWindow.Game));
+                memCards.Add(new Card(x, y, 140, 100, "CARD"));
                 x += 150;
 
                 if ((i + 1) % 4 == 0)
@@ -98,13 +121,13 @@ namespace Memory
                 }
             }
 
-            var image = rl.LoadTexture("Solution Items\\emoji.png");
+            var image = LoadTexture("slika.png");
 
-            while (!rl.WindowShouldClose())
+            while (!WindowShouldClose())
             {
-                rl.BeginDrawing();
+                BeginDrawing();
 
-                rl.ClearBackground(Color.WHITE);
+                ClearBackground(Color.WHITE);
 
                 switch (gameWindow)
                 {
@@ -117,19 +140,18 @@ namespace Memory
                     case GameWindow.Options:
                         break;
                     case GameWindow.Quit:
-                        rl.EndDrawing();
-                        rl.CloseWindow();
+                        EndDrawing();
+                        CloseWindow();
                         Environment.Exit(1);
                         break;
                 }
 
-
-                rl.EndDrawing();
+                EndDrawing();
 
                 System.Threading.Thread.Sleep(10);
             }
 
-            rl.CloseWindow();
+            CloseWindow();
         }
     }
 }
